a064de6f294c2cf32169b0a7da3ee72e9192248b
yraffic02 <yraffic02@gmail.com>
2024-02-22 14:41:06 -0300

    chore: criado schema registre

--- hands-on/src/components/Forms/FormRegistre/index.tsx
+++ hands-on/src/components/Forms/FormRegistre/index.tsx
@@ -1,78 +1,72 @@
 import { Button } from "@/components/Button"
 import { Input } from "@/components/Input"
 import { Label } from "@/components/Label"
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import Link from "next/link"
+import { TypeFormRegistre, schemaRegistre } from "@/utils/schema/shema.form-registre";
 
 export const FormRegistre = () =>{
-    return(
-        <form className="flex flex-col gap-8">
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    mode: 'all',
+    reValidateMode: 'onChange',
+    resolver: yupResolver(schemaRegistre)
+  })
 
-            <h1
-              className="text-[28px] font-medium text-[#7978D9] text-center"
-            >
-              Cadastre-se
-            </h1>
+  const onSubmit: SubmitHandler<TypeFormRegistre> = async (data) => {
+    console.log(data)
+  }
 
-            <div className="flex flex-col gap-2">
-              <Label
-                labelHtmlFor="name"
-                type="sm"
-              >
-                Nome
+  return(
+      <form className="flex flex-col gap-8">
+          <h1
+            className="text-[28px] font-medium text-[#7978D9] text-center"
+          >
+            Cadastre-se
+          </h1>
+
+          <div className="flex flex-col gap-2">
+              <Label labelHtmlFor="name" type="sm">
+                  Nome
               </Label>
-              <Input
-                id="name"
-              />
-            </div>
+              <Input id="name" {...register('name')} />
+              {errors.name && <span className="text-red-500 text-xs">{errors.name.message}</span>}
+          </div>
 
-            <div className="flex flex-col gap-2">
-              <Label
-                labelHtmlFor="email"
-                type="sm"
-              >
-                E-mail
+          <div className="flex flex-col gap-2">
+              <Label labelHtmlFor="email" type="sm">
+                  E-mail
               </Label>
-              <Input
-                id="email"
-              />
-            </div>
+              <Input id="email" {...register('email')} />
+              {errors.email && <span className="text-red-500 text-xs">{errors.email.message}</span>}
+          </div>
 
-            <div className="flex flex-col gap-2">
-              <Label
-                labelHtmlFor="password"
-                type="sm"
-              >
-                Senha
+          <div className="flex flex-col gap-2">
+              <Label labelHtmlFor="password" type="sm">
+                  Senha
               </Label>
-              <Input
-                id="password"
-                type="password"
-              />
-            </div>
+              <Input id="password" type="password" {...register('password')} />
+              {errors.password && <span className="text-red-500 text-xs">{errors.password.message}</span>}
+          </div>
 
-            <div className="flex flex-col gap-2">
-              <Label
-                labelHtmlFor="confirme"
-                type="sm"
-              >
-                Confirmação de senha
+          <div className="flex flex-col gap-2">
+              <Label labelHtmlFor="confirmPassword" type="sm">
+                  Confirmação de senha
               </Label>
-              <Input
-                id="confirme"
-                type="password"
-              />
-            </div>
+              <Input id="confirmPassword" type="password" {...register('confirmPassword')} />
+              {errors.confirmPassword && <span className="text-red-500 text-xs">{errors.confirmPassword.message}</span>}
+          </div>
 
-            <Button>
-              Cadastrar
-            </Button>
+          <Button>
+            Cadastrar
+          </Button>
 
-            <Link
-              href='/'
-              className="text-[#7978D9] text-center"
-            >
-              Já tem cadastro? Clique aqui!
-            </Link>
-        </form>
-    )
+          <Link
+            href='/'
+            className="text-[#7978D9] text-center"
+          >
+            Já tem cadastro? Clique aqui!
+          </Link>
+      </form>
+  )
 }
