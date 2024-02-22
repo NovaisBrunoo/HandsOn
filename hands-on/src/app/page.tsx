b90f98a9e18f583b9df94a47fdd83efcb1c4fa2a
yraffic02 <yraffic02@gmail.com>
2024-02-21 23:09:51 -0300

    feat: criado page login

--- hands-on/src/app/page.tsx
+++ hands-on/src/app/page.tsx
@@ -1,8 +1,91 @@
+import { Input } from "@/components/Input";
+import { Label } from "@/components/Label";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[url('/background.png')] bg-cover bg-no-repeat h-screen w-screen">
+      <header className="flex items-center justify-start py-10 px-[105px]">
+        <div className="flex items-center gap-1 cursor-pointer">
+            <Image 
+              src='/logo.svg'
+              height={45}
+              width={45}
+              alt="logo dindin"
+            />
+            <h1 className="text-white font-bold text-4xl">
+              Dindin
+            </h1>
+        </div>
+      </header>
 
+      <div className="flex items-center justify-evenly">
+        <div className="w-1/2 flex flex-col gap-9 text-white">
+            <h1
+                className="text-[52px] font-bold"
+              >
+                Controle suas <b className="text-[#7978D9]">finanças</b>, sem planilha chata.
+              </h1>
+            <h2 
+              className="text-[28px] font-normal"
+            >
+                Organizar as suas finanças nunca foi tão fácil,
+                com o DINDIN, você tem tudo num único lugar e 
+                em um clique de distância.
+              </h2>
+              <Link href='/home'>
+                <div className="w-72">
+                  <button className="bg-[#7978D9] rounded py-3 w-full">
+                    Cadastre-se
+                  </button>
+                </div>
+              </Link>
+        </div>
+
+        <div 
+          className="bg-white px-8 py-11 flex flex-col w-[513px]"
+        >
+          <form className="flex flex-col gap-8">
+            <div className="flex items-center justify-center w-full">
+              <h1 
+                className="text-[28px] font-medium text-[#7978D9]"
+              >
+                Login
+              </h1>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label
+              labelHtmlFor="email"
+              type="sm"
+              >
+                E-mail
+              </Label>
+              <Input 
+                id="email"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label
+              labelHtmlFor="password"
+              type="sm"
+              >
+                Password
+              </Label>
+              <Input 
+                id="password"
+                type="password"
+              />
+            </div>
+
+            <button className="bg-[#7978D9] rounded py-3 w-full text-white mt-10">
+                    Cadastre-se
+            </button>
+          </form>
+        </div>
+      </div>
     </main>
   );
 }
